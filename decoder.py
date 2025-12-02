2ea5d400d34a1193314ac2d2a7399dfd9ec80510
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-12-02 13:34:48 +0530

    feat: add audio decoder for binary signal to text conversion
    - Implement decode_bit function to interpret audio chunks as bits
    - Add decode_signal function to convert a full audio signal to text
    - Define constants for sample rate, duration, and frequencies

---
+++ decoder.py
@@ -0,0 +1,48 @@
+import numpy as np
+SAMPLE_RATE = 44100
+DURATION = 0.08
+FREQ_0 = 17500
+FREQ_1 = 18500
+
+def decode_bit(chunk):
+    """
+    Decode a single chunk of audio into '0' or '1'
+    """
+
+    #computing fft of the chunk
+    fft = np.fft.rfft(chunk)
+    freqs = np.fft.rfftfreq(len(chunk), 1 / SAMPLE_RATE)
+
+    #finding the frequency with maximum magnitude
+    peak_idx = np.argmax(np.abs(fft))
+    peak_freq = freqs[peak_idx]
+
+    #determining the nearest frquency
+    if abs(peak_freq - FREQ_1) < abs(peak_freq - FREQ_0):
+        return '1'
+    else:
+        return '0'
+    
+def decode_signal(signal):
+    """
+    Decode a full audio signal into a single text msg
+    """
+    sample_per_bit = int(SAMPLE_RATE*DURATION)
+    bits = ""
+
+    for i in range(0, len(signal), sample_per_bit):
+        chunk = signal[i:i + sample_per_bit]
+        if len(chunk) < sample_per_bit:
+            break
+        bits += decode_bit(chunk)
+
+    
+    #cinvert bits to characters
+    msg = ""
+    for i in range(0, len(bits),8):
+        byte = bits[i:i+8]
+        if len(byte) == 8:
+            msg += chr(int(byte,2))
+            
+    return msg
+    
