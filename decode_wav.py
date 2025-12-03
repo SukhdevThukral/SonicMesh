8921ae1b4f3059bc048d71a270e12488276d3e12
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-12-03 13:47:22 +0530

    feat: add bitstream to text conversion in WAV decoder
    - Implemented bits_to_text function to convert bitstream to ASCII text
    - Updated decode_wav to output decoded message as readable text
    - Renamed variable bits_msg to bits for clarity
    - Modified user input prompt for output filename to strip whitespace

--- decode_wav.py
+++ decode_wav.py
@@ -2,6 +2,16 @@ import soundfile as sf
 import numpy as np
 from decoder import decode_signal, decode_file
 
+def bits_to_text(bitstream):
+    """"Convert a given bitstream into readable ASCII text"""
+    text = ""
+    for i in range(0, len(bitstream),8):
+        byte = bitstream[i:i+8]
+        if len(byte) == 8:
+            text+=chr(int(byte,2))
+    return text
+    
+
 def decode_wav(as_file=False, output_file=None):
     """
     Decode a WAV file saved from SonicMesh.
@@ -19,23 +29,21 @@ def decode_wav(as_file=False, output_file=None):
 
     data, sr = sf.read(wav_path)
 
-    if sr != 44100:
-        print(f"[WARNING] WAV file sample rate is {sr}, expected 44100")
-
     #flattening in case if its stereo
     signal = np.array(data).flatten()
 
     print("[SonicMesh] Decoding...")
-    bits_msg  = decode_signal(signal)
+    bits = decode_signal(signal)
 
     if as_file == True:
         if output_file== None:
             output_file = "received_file.bin"
-        decode_file(bits_msg, output_file)
+        decode_file(bits, output_file)
         print("File decoded and saved to", output_file)
     else:
+        text_msg = bits_to_text(bits)
         print("\n==========================")
-        print(" DECODED MESSAGE: ", bits_msg)
+        print(" DECODED bitsteam: ", text_msg)
         print("\n==========================")
 
 if __name__ == "__main__":
@@ -43,7 +51,7 @@ if __name__ == "__main__":
     if choice == "t":
         decode_wav(as_file=False)
     elif choice == "f":
-        output_file = input("save output as: ")
+        output_file = input("Save output as: ").strip()
         decode_wav(as_file=True, output_file=output_file)
     else:
         print("Invalid choice")
