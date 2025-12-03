ccf1be078012f4ab81077d750be1f445e0706da2
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-12-03 14:00:18 +0530

    chore:auto-commit remaining files

--- encoder.py
+++ encoder.py
@@ -2,16 +2,15 @@ import numpy as np
 import zlib
 from acoustic_config import SAMPLE_RATE, SYMBOL_DURATION, AMPLITUDE, FREQ_TABLE, CHUNK_SIZE 
 
-# ===========================
-#  encoding a symbol (4bits)
-# ===========================
 
-def encode_symbol(bits4):
+bits_per_symbol = 5
+
+def encode_symbol(bits5):
     """
-    encode 4 bits (string) into an ultrasonic tone.
+    encode 5 bits (string) into an ultrasonic tone.
     """
 
-    idx = int(bits4, 2)
+    idx = int(bits5, 2)
     freq = FREQ_TABLE[idx]
 
     t = np.linspace(0, SYMBOL_DURATION, int(SAMPLE_RATE*SYMBOL_DURATION), endpoint=False)
@@ -19,12 +18,12 @@ def encode_symbol(bits4):
     tone = AMPLITUDE*np.sin(2*np.pi*freq*t)
     return tone
     
-def encode_bits_16fsk(bitstream):
+def encode_bits_fsk(bitstream):
     tones = []
-    for i in range(0, len(bitstream),4):
-        chunk = bitstream[i:i+4]
-        if len(chunk) < 4:
-            chunk = chunk.ljust(4,'0') #pad
+    for i in range(0, len(bitstream), bits_per_symbol):
+        chunk = bitstream[i:i+bits_per_symbol]
+        if len(chunk) < bits_per_symbol:
+            chunk = chunk.ljust(bits_per_symbol,'0') #pad
         tone = encode_symbol(chunk)
         tones.append(tone)
 
@@ -82,7 +81,7 @@ def packets_to_bits(packets):
 def encode_file(path):
     packets = packetize_file(path)
     bitstream = packets_to_bits(packets)
-    audio = encode_bits_16fsk(bitstream)
+    audio = encode_bits_fsk(bitstream)
     return audio
 
 # text msg encoder
@@ -94,8 +93,8 @@ def encode_message(msg):
         bitstream += f"{ord(char):08b}"
 
     # pad for 16-FSK
-    if len(bitstream) % 4 != 0:
-        pad_len = 4 - (len(bitstream) % 4)
+    if len(bitstream) % bits_per_symbol != 0:
+        pad_len = bits_per_symbol - (len(bitstream) % bits_per_symbol)
         bitstream += "0" * pad_len
 
-    return encode_bits_16fsk(bitstream)
+    return encode_bits_fsk(bitstream)
