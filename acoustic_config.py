57af2c17ec73d65edec879c5ca9e3a42c8a7c2fb
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-12-03 18:47:51 +0530

    feat: update acoustic configuration for improved performance and clarity
    - Reduce symbol bits from 6 to 5 and adjust frequency table range
    - Increase chunk size for packet processing
    - Add windowing function and silence duration for FFT detection
    - Update amplitude and symbol duration for reduced audibility
    
    feat(decoder): enhance FSK decoding with improved silence detection
    
    - Modify decode_symbol to return None for low amplitude signals
    - Refactor decode_signal to normalize chunks before processing
    - Update decode_file to properly handle bitstream to byte conversion
    - Adjust packet parsing to reflect changes in received data handling

--- acoustic_config.py
+++ acoustic_config.py
@@ -2,21 +2,31 @@
 #  SonicMesh Acoustic Constants
 # ===============================
 
+import numpy as np 
+
+
 SAMPLE_RATE = 44100
 
-#symbol = 6bits
+# SYMBOL PROPERTIES
+SYMBOL_BITS = 5
+SYMBOL_DURATION = 0.015 #10ms per symbol
+AMPLITUDE = 0.35  # AUDIO APMLITUDE
 
-SYMBOL_BITS = 6 
-SYMBOL_DURATION = 0.015 #2ms per symbol
-AMPLITUDE = 0.25 #AUDIO APMLITUDE
+# FFT optimization
+WINDOW_OVERLAP = 0.5 #50% overlap for FFT windows
+WINDOW_FUNCTION = np.hamming # windowing function to reduce spectral leakage
 
 #*64-FSK ultrasonic frequencies (mostly inaudible)
 # keeping above ~20kHz to reduce audiblity, upper limit ~22kHz for most speakers
-import numpy as np 
-FREQ_TABLE = np.linspace(20050, 22000, 64,dtype=int).tolist()
+FREQ_TABLE = np.linspace(17500, 20500, 32 ,dtype=int).tolist()
 
 # packet chunk size
-CHUNK_SIZE = 32
+CHUNK_SIZE = 64
 
+# audio post-processing
+SILENCE_BETWEEN_PACKETS = 0.004 # 4 ms silence to seperate packets for FFT detection
 
-# switched to 64-fsk -> 6 bits per symbol
+# Note:
+# reduced symbol duration + slight overlap reduces total audio length
+# lower amplitude + higher start frequency reduces the audiblity even further.
+# window function and silence help reduce CRC mismatches
