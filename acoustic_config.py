b3e5b8703970be3bb88e573900ce3fca985a5e6f
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-12-04 11:18:53 +0530

    feat: adjust acoustic parameters for improved audio performance
    - decrease symbol duration to enhance transmission speed
    - increase window overlap for better FFT analysis
    - shift frequency table start to reduce audibility
    - maintain silence between packets for optimal detection
    
    feat(decoder): enhance synchronization detection in bitstream decoding
    
    - Improve SYNC_BITS generation using dynamic bit width.
    - Implement a new `find_sync` function for symbol-aligned SYNC search.
    - Add tone magnitude check to discard low signal peaks during decoding.
    - Refactor SYNC search process to fallback on naive search if needed.

--- acoustic_config.py
+++ acoustic_config.py
@@ -9,23 +9,22 @@ SAMPLE_RATE = 44100
 
 # SYMBOL PROPERTIES
 SYMBOL_BITS = 6
-SYMBOL_DURATION = 0.01 # 2.5x faster
+SYMBOL_DURATION = 0.006 # 2.5x faster
 AMPLITUDE = 0.45  # AUDIO APMLITUDE
 
 # FFT optimization
-WINDOW_OVERLAP = 0.25
+WINDOW_OVERLAP = 0.4
 WINDOW_FUNCTION = np.hanning # windowing function to reduce spectral leakage
 
 #*64-FSK ultrasonic frequencies (mostly inaudible)
 # keeping above ~20kHz to reduce audiblity, upper limit ~22kHz for most speakers
-FREQ_TABLE = np.linspace(15000, 22000, 64 ,dtype=int).tolist()
+FREQ_TABLE = np.linspace(19000, 22000, 64 ,dtype=int).tolist()
 
 # packet chunk size
 CHUNK_SIZE = 800
 
 # audio post-processing
-SILENCE_BETWEEN_PACKETS = 0 # 4 ms silence to seperate packets for FFT detection
-
+SILENCE_BETWEEN_PACKETS = 0 
 # Note:
 # reduced symbol duration + slight overlap reduces total audio length
 # lower amplitude + higher start frequency reduces the audiblity even further.
